e94e0fc67fd90acdca638f9eff268cd3399972d0
Michael Mansperger <ManspergerMichael@gmail.com>
2019-02-26 21:34:37 -0800

    JS finished

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ window.addEventListener('online',() => {
 });
 
 window.addEventListener('offline',() => {
-    
+    callNotification('offline', 'offline','&#x26A0')
 });
 
 function callNotification(type, statusText, iconHtml){
-    const notification = document.getElementById('noteification')
+    const notification = document.getElementById('noteification');
+    const status = document.getElementById('status');
+    const icon = document.getElementById('icon');
+    status.innerText = statusText;
+    icon.innerHTML = iconHtml;
+
+    notification.classList.remove('offline','online');
+
+    notification.className = type;
 }
