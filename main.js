3163448adb709b1c58c64b6cb2e8f5823b763ce3
Michael Mansperger <ManspergerMichael@gmail.com>
2019-02-26 21:26:20 -0800

    css finished JS started

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener('online',() => {
+
+});
+
+window.addEventListener('offline',() => {
+    
+});
+
+function callNotification(type, statusText, iconHtml){
+    const notification = document.getElementById('noteification')
+}
